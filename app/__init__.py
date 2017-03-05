ad3951221deb2e58a43ee7eb11fb7f5581eb13d6
andrewsosa001 <andrewsosa001@gmail.com>
2017-03-05 15:45:34 -0500

    init commit

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+
+@app.route('/api/words', methods=['POST'])
+def main():
+
+    # default keywords
+    keywords = ['uh', 'uhm']
+
+    # parse data
+    text = request.form.get('text','')
+    r_keywords = request.form.get('keywords', None)
+
+    if r_keywords:
+        keywords = r_keywords.split(',')
+        keywords = [k.strip() for k in keywords]
+
+    # analyze text
+    occurances = dict()
+    for word in keywords:
+        occurances[word] = text.count(word)
+
+    # send back
+    return jsonify(occurances)
